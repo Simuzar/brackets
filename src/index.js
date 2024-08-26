4ffe3fff7c19aa25a597742f45388698ee436d92
Simuzar <fatulazade96@gmail.com>
2024-08-26 21:22:36 +0300

    docs: update solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+let stack = [];
+const objToCompare = {};
+
+bracketsConfig.forEach(pair => {
+  objToCompare[pair[1]] = pair[0];
+});
+
+let openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+
+for(let char of str) {
+  if(openingBrackets.has(char)) {
+    if(objToCompare.char === char) {
+      stack[stack.length - 1] ===  char ? stack.pop() : stack.push(char);
+      } else {
+        stack.push(char);
+      }
+    } else if (stack.length === 0 || stack[stack.length - 1] !==  objToCompare[char] ) {
+      return false;
+    } else {
+      stack.pop();
+    }
+  } 
+return stack.length === 0;
 }
+
+
+
